5727bbb7f1dc7ce09d5041854b982565790d8351
gmf <mg3534@columbia.edu>
2015-02-15 19:15:16 -0500

    Add query Class

---
+++ Bing-Enhance/src/util/Query.java
@@ -0,0 +1,20 @@
+package util;
+
+public class Query {
+	public String[] tokens = null;
+	public double[] weight = null;
+	
+	
+//	public static void main(String[] args){
+//		String s = "columbia";
+//		Query q = new Query();
+//		q.tokens=s.split(" ");
+//		for(int i=0;i<q.tokens.length;i++)
+//			System.out.println(q.tokens[i]);
+//		
+//		s = "Columbia%University%Engineering";
+//		q.tokens=s.split("%");
+//		for(int i=0;i<q.tokens.length;i++)
+//			System.out.println(q.tokens[i]);
+//	}
+}
